a40564daa067d3cac878d9ac5896f77a868d709c
Semih <semih2001aras@hotmail.com>
2022-01-30 13:33:25 +0300

    update network_constant.dart

--- lib/core/constants/network/network_constant.dart
+++ lib/core/constants/network/network_constant.dart
@@ -1,6 +1,5 @@
 class NetworkConstant {
   static const BASE_URL = "https://api.pexels.com/v1/";
-  static const API_KEY =
-      "563492ad6f9170000100000162d72a5ca0764c7086ca59eb36cd11dc";
+  static const API_KEY = "YOUR_PEXELS_API_KEY";
   static const HEADERS = {"Authorization": API_KEY};
 }
