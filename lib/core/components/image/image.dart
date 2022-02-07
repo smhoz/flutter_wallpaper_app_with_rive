ef56b58bb0d51eb8f66b58ddd49561c93b6975bb
Semih <semih2001aras@hotmail.com>
2022-02-07 11:20:16 +0300

    Update image

--- lib/core/components/image/image.dart
+++ lib/core/components/image/image.dart
@@ -24,12 +24,24 @@ class CustomNetworkImage extends StatelessWidget {
               borderRadius: CustomBorderRadius.allCircular(),
               child: Image.network(
                 imageURL,
-                loadingBuilder: WidgetsConstant.instance.loadingBuilder,
+                loadingBuilder: loadingBuilder,
                 fit: BoxFit.cover,
               )),
         ),
       ),
     );
   }
+
+  Widget loadingBuilder(
+      BuildContext context, Widget child, ImageChunkEvent? loadingProgress) {
     if (loadingProgress == null) return child;
+    return Center(
+      child: CircularProgressIndicator(
+        value: loadingProgress.expectedTotalBytes != null
+            ? loadingProgress.cumulativeBytesLoaded /
+                loadingProgress.expectedTotalBytes!
+            : null,
+      ),
+    );
+  }
 }
